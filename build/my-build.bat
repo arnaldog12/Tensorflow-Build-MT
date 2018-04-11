507fc375aa29561d66cf419d84a16c6a0bf2238a
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2018-04-11 10:25:15 -0300

    build v1.4 with /MT
    in v1.4, there is a bug with tf_stream_executor.vcxproj (as reported by this video: https://www.youtube.com/watch?v=gj_4Yv94LgQ).
    
    Thus, before the MSBuild command, I had to update such project. This update involves add the following path:
    C:\tensorflow\third_party\toolchains\gpus\cuda;
    to the end of Additional Include Directories of configuration Release x64.
    
    The instructions are also in the video.

--- build/my-build.bat
+++ build/my-build.bat
@@ -22,18 +22,17 @@ ECHO ON
 
 :: Set environment variables to be shared between runs. Do not override if they
 :: are set already.
+IF DEFINED CMAKE_EXE (ECHO CMAKE_EXE is set to %CMAKE_EXE%) ELSE (SET CMAKE_EXE="C:/Program Files/CMake/bin/cmake.exe")
+IF DEFINED SWIG_EXE (ECHO SWIG_EXE is set to %SWIG_EXE%) ELSE (SET SWIG_EXE="C:/tools/swigwin-3.0.10/swig.exe")
+IF DEFINED PY_EXE (ECHO PY_EXE is set to %PY_EXE%) ELSE (SET PY_EXE="C:/Users/administrator/AppData/Local/Continuum/miniconda3/envs/python35/python.exe")
+IF DEFINED PY_LIB (ECHO PY_LIB is set to %PY_LIB%) ELSE (SET PY_LIB="C:/Users/administrator/AppData/Local/Continuum/miniconda3/envs/python35/libs/python35.lib")
 
-IF DEFINED CMAKE_EXE (ECHO CMAKE_EXE is set to %CMAKE_EXE%) ELSE (SET CMAKE_EXE="C:\Program Files\CMake\bin\cmake.exe")
-IF DEFINED SWIG_EXE (ECHO SWIG_EXE is set to %SWIG_EXE%) ELSE (SET SWIG_EXE="C:\swigwin\swig.exe")
-IF DEFINED PY_EXE (ECHO PY_EXE is set to %PY_EXE%) ELSE (SET PY_EXE="C:\ProgramData\Anaconda3\python.exe")
-IF DEFINED PY_LIB (ECHO PY_LIB is set to %PY_LIB%) ELSE (SET PY_LIB="C:\ProgramData\Anaconda3\libs\python35.lib")
-IF DEFINED CUDNN_HOME (ECHO CUDNN_HOME is set to %CUDNN_HOME%) ELSE (SET CUDNN_HOME="C:\cudnn_6.0_cuda_8")
-verbosity:quiet
-SET CMAKE_DIR=%REPO_ROOT%\tensorflow\contrib\cmake
-SET MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+:: verbosity:quiet
+SET CMAKE_DIR=C:/tensorflow/tensorflow/contrib/cmake
+SET MSBUILD_EXE="C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/MSBuild/15.0/Bin/MSBuild.exe"
 
 :: Run cmake to create Visual Studio Project files.
-%CMAKE_EXE% %CMAKE_DIR% -A x64 -DSWIG_EXECUTABLE=%SWIG_EXE% -DPYTHON_EXECUTABLE=%PY_EXE% -DCMAKE_BUILD_TYPE=Release -DPYTHON_LIBRARIES=%PY_LIB% -Dtensorflow_BUILD_PYTHON_BINDINGS=OFF
+%CMAKE_EXE% %CMAKE_DIR% -A x64 -DSWIG_EXECUTABLE=%SWIG_EXE% -DPYTHON_EXECUTABLE=%PY_EXE% -DCMAKE_BUILD_TYPE=Release -DPYTHON_LIBRARIES=%PY_LIB% -Dtensorflow_BUILD_PYTHON_BINDINGS=OFF -DCMAKE_CXX_FLAGS_DEBUG="/MTd /Zi /Ob0 /Od /RTC1" -DCMAKE_CXX_FLAGS_MINSIZEREL="/MT /O1 /Ob1 /DNDEBUG" -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /Ob2 /DNDEBUG" -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="/MT /Zi /O2 /Ob1 /DNDEBUG" -DCMAKE_C_FLAGS_DEBUG="/MTd /Zi /Ob0 /Od /RTC1" -DCMAKE_C_FLAGS_MINSIZEREL="/MT /O1 /Ob1 /DNDEBUG" -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /Ob2 /DNDEBUG" -DCMAKE_C_FLAGS_RELWITHDEBINFO="/MT /Zi /O2 /Ob1 /DNDEBUG"
 
-:: Run msbuild in the resulting VS project files to build a pip package.
-:: %MSBUILD_EXE% /p:Configuration=Release /p:Platform=x64 /m:6 tensorflow.sln /p:PreferredToolArchitecture=x64
+:: Run msbuild in the resulting VS project files to build a pip package (needs to fix tf_stream_executor.vcxproj first).
+:: %MSBUILD_EXE% /p:Configuration=Release /p:Platform=x64 /m:6 tensorflow.sln /t:Clean;Build /p:PreferredToolArchitecture=x64
